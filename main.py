748baa9a5805e7e039853a6eb21674bcaa492f5f
srush3 <129663796+srush3@users.noreply.github.com>
2023-04-09 15:38:42 +0300

    v1.1
    добавил 2 ракетки

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image) , (65, 65))
+        self.image = transform.scale(image.load(player_image) , (width, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -13,16 +13,23 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys_pressed[K_s] and self.rect.y < 430:
             self.rect.y += self.speed
-        if keys_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < 630:
-            self.rect.x += self.speed
+
+    def update_r(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < 430:
+            self.rect.y += self.speed
+
+
+hero1 = Player("rocet.jpg", 5, 50, 5, 30, 100)
+hero2 = Player("rocet.jpg", 675, 450, 5, 30, 100)
 
 clock = time.Clock()
 window = display.set_mode((700, 500))
@@ -31,9 +38,15 @@ game = True
 while game:
     window.fill((200,255,255))
 
+    hero1.update_l()
+    hero1.reset()
+    hero2.update_r()
+    hero2.reset()
+
     for e in event.get():
         if e.type == QUIT:
             game = False
+
 
 
     display.update()
