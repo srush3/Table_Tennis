c5392acf4988a7ecd695c0a62e8ee42c10dda8df
srush3 <129663796+srush3@users.noreply.github.com>
2023-04-09 15:15:11 +0300

    v1.0
    Добавил окно с голубым фоном и закрытие окна на крестик

---
+++ main.py
@@ -0,0 +1,40 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image) , (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 430:
+            self.rect.y += self.speed
+        if keys_pressed[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys_pressed[K_d] and self.rect.x < 630:
+            self.rect.x += self.speed
+
+clock = time.Clock()
+window = display.set_mode((700, 500))
+
+game = True
+while game:
+    window.fill((200,255,255))
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    display.update()
+    clock.tick(60)
