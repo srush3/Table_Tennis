3450f0d409927e75ca7b2b8813eff30a1b2b7e76
srush3 <129663796+srush3@users.noreply.github.com>
2023-04-16 15:32:15 +0300

    v1.3

--- main.py
+++ main.py
@@ -30,20 +30,35 @@ class Player(GameSprite):
 
 hero1 = Player("rocet.jpg", 5, 50, 5, 30, 100)
 hero2 = Player("rocet.jpg", 675, 450, 5, 30, 100)
-ball =GameSprite("shar.jpg", 300,300, 5,15, 15)
+ball =GameSprite("shar.jpg", 300,50, 5,15, 15)
 speed_x = 3
 speed_y = 3
 
 clock = time.Clock()
 window = display.set_mode((700, 500))
+font.init()
+font1 = font.Font(None, 30)
+lose1 = font1.render("Первый игрок проиграл", True, (0, 0, 0))
+lose2 = font1.render("Второй игрок проиграл", True, (0, 0, 0))
+
+
 
 game = True
 while game:
     ball.rect.y += speed_y
     ball.rect.x += speed_x
+    if sprite.collide_rect(ball, hero1) or sprite.collide_rect(ball, hero2):
+        speed_x *= -1
+    if ball.rect.y > 500-15 or ball.rect.y < 0:
+        speed_y *= -1
+    window.fill((200, 255, 255))
+    if ball.rect.x < 0:
+        window.blit(lose1, (200, 200))
+    if ball.rect.x > 700 - 15:
+        window.blit(lose2, (200, 200))
 
 
-    window.fill((200,255,255))
+
 
     hero1.update_l()
     hero1.reset()
